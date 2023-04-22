cb99186ea59adf443b305cda13eb571861622e3f
Will Haynes <will@broadsheet.technology>
2023-04-22 11:12:56 -0500

    v0.1: initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# 1/ configure new-relic php agent
+update_newrelic_config() {
+    local license_key="$1"
+    local app_name="$2"
+    if [[ -n "$license_key" ]]; then
+        sed -i -e "s/REPLACE_WITH_REAL_KEY/$license_key/" "$(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini"
+        if [[ -n "$app_name" ]]; then
+            sed -i -e "s/newrelic.appname.*/newrelic.appname=\"$app_name\"/" "$(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini"
+        fi
+    else
+        echo "No NRIA_LICENSE_KEY found. Skipping New Relic configuration"
+    fi
+}
+update_newrelic_config "$NRIA_LICENSE_KEY" "$NRIA_APP_NAME"
+
+# 2/ dump ENV variables into cron environment
+printenv > /etc/environment
+
+# 3/ generate php-fpm.conf and php.ini from environment variables
+cat <<EOF > /usr/local/etc/php-fpm.d/zzz-broadsheet.technology-wordpress-extra.conf
+[www]
+pm = ${BT_PHP_PM:-dynamic}
+pm.max_children = ${BT_PHP_PM_MAX_CHILDREN:-10}
+pm.max_requests = ${BT_PHP_PM_MAX_REQUESTS:-200}
+EOF
+
+cat <<EOF > /usr/local/etc/php/conf.d/zzz-broadsheet.technology-wordpress-extra.ini
+file_uploads = On
+memory_limit = 264M
+upload_max_filesize = ${BT_PHP_UPLOAD_MAX_FILESIZE:-6M}
+post_max_size = 64M
+max_execution_time = 600
+
+opcache.max_accelerated_files=${BT_PHP_OPCACHE_MAX_ACCELERATED_FILES}
+opcache.memory_consumption=${BT_PHP_OPCACHE_MEMORY_CONSUMPTION}
+EOF
+
+# 4/ start cron service
+service cron start
